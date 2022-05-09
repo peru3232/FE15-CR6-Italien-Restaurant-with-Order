76f1306e7f25923c592cf1d8b4816afaae11f788
peru3232 <prw@qmail.com.de>
2022-05-09 08:48:23 +0200

    add assignment

--- src/app/dishes.ts
+++ src/app/dishes.ts
@@ -12,7 +12,7 @@ export const dishes = [
     name: 'Antipasto Caldo e Freddo',
     image: '/assets/images/antipasto-caldo-e-freddo.jpg',
     description: "Ingredients: fish and mussels variety, cooked (bioled, fried or baked) with diverse herbs and sauces. Please contact our chef to find out more.",
-    addInfo: "Use the unique chance to taste our chef's suggestion. Best enjoyed with our white wine, this dish is a composition of hot and cold fish and mussels variety, which may differ due to the season and thus surprise the most sophisticated gourmet. From baked scallops, oysters and blue mussels to shrimps and sturgeon quiche - the magnificence of this dish won't disappoint You!",
+    addInfo: "Use the unique chance to taste our chef`s suggestion. Best enjoyed with our white wine, this dish is a composition of hot and cold fish and mussels variety, which may differ due to the season and thus surprise the most sophisticated gourmet. From baked scallops, oysters and blue mussels to shrimps and sturgeon quiche - the magnificence of this dish won`t disappoint You!",
     price: 24.90,
     available: false,
     qtty: 1
@@ -30,16 +30,16 @@ export const dishes = [
     name: 'Pizza Diavola',
     image: '/assets/images/diavola.jpg',
     description: 'Ingredients: wheat pizza crust, tomato-chili-oregano sauce, mozzarella cheese, salami, red onions, pepperoni, Italian herbs.',
-    addInfo: "Who believes pizza diavola to be nothing special, hasn't tasted our favourite creation. Inside of the traditional stone oven, a perfect composition of a crispy middle-thin pizza crust and a characteristic spicy topping appears. The combination of a special Italian hot salami, pepperoni, red onions and tasty self-made pizza sauce (cooked after grandmother's recipe for about five hours) is absolutely irresistible and strongly recommended for those, who are in search of savoury impressions.",
+    addInfo: "Who believes pizza diavola to be nothing special, hasn`t tasted our favourite creation. Inside of the traditional stone oven, a perfect composition of a crispy middle-thin pizza crust and a characteristic spicy topping appears. The combination of a special Italian hot salami, pepperoni, red onions and tasty self-made pizza sauce (cooked after grandmother`s recipe for about five hours) is absolutely irresistible and strongly recommended for those, who are in search of savoury impressions.",
     price: 11.90,
     available: true,
     qtty: 1
   },
   {
-    name: 'Profiteroles ',
+    name: 'Profiteroles',
     image: '/assets/images/profiteroles.jpg',
     description: 'Ingredients: wheat puff dough, whipped cream, dark chocolate topping (with or without alcohol addition).',
-    addInfo: "This typical French dessert would perfectly complete any meal. Best served with coffee at lunch time and grappa in the evening, it's a light and tasty creation, consisting of puff pastry, filled with whipped cream and garnished with dark chocolate.",
+    addInfo: "This typical French dessert would perfectly complete any meal. Best served with coffee at lunch time and grappa in the evening, it`s a light and tasty creation, consisting of puff pastry, filled with whipped cream and garnished with dark chocolate.",
     price: 7.90,
     available: true,
     qtty: 1
